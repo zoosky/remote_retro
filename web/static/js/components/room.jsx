3692aef34c4ff22a22f08a91e6eaf06f1d1b5f92
Travis Vander Hoop <vanderhoop@me.com>
2019-03-19 15:06:36 -0400

    Render custom grouping stage component
      - reuse idea_generation_styles in grouping stage

--- web/static/js/components/room.jsx
+++ web/static/js/components/room.jsx
@@ -2,12 +2,13 @@ import React from "react"
 import PrimeDirectiveStage from "./prime_directive_stage"
 import LobbyStage from "./lobby_stage"
 import IdeaGenerationStage from "./idea_generation_stage"
+import GroupingStage from "./grouping_stage"
 import stageConfigs from "../configs/stage_configs"
 import STAGES from "../configs/stages"
 
 import * as AppPropTypes from "../prop_types"
 
-const { LOBBY, PRIME_DIRECTIVE } = STAGES
+const { LOBBY, PRIME_DIRECTIVE, GROUPING } = STAGES
 
 const Room = props => {
   let roomContents
@@ -25,6 +26,14 @@ const Room = props => {
   } else if (stage === PRIME_DIRECTIVE) {
     roomContents = (
       <PrimeDirectiveStage
+        {...props}
+        progressionConfig={stageConfig}
+        isFacilitator={isFacilitator}
+      />
+    )
+  } else if (stage === GROUPING) {
+    roomContents = (
+      <GroupingStage
         {...props}
         progressionConfig={stageConfig}
         isFacilitator={isFacilitator}
